69788b8445a4805954ba2b608f89ef36f0e3bdba
Giorgi Pilishvili <giorgi.pilishvili.1@btu.edu.ge>
2023-04-24 21:13:17 +0400

    JAVA

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,16 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {1, 3, 21, 5, -2};
-
-        int result = SumArray.sumNumbersOfArray(numbers);
-        System.out.println(result);
-
         File file = new File("src/numbers.txt");
 
         try {
+
             int[] numbersFromFile = SumArray.readNumbersFromFile(file);
             System.out.println(Arrays.toString(numbersFromFile));
+
+            int sum =  SumArray.sumNumbersOfArray(numbersFromFile);
+            System.out.println(sum);
+
         } catch (FileNotFoundException error) {
             System.out.println("Invalid to read file. " + error.getMessage());
         } catch (InvalidNumberException error) {
